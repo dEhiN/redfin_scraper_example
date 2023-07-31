d2138220de45b766ce78a7a222ce7480894f8bc4
David H. Watson <hiranwatson@gmail.com>
2023-07-31 00:25:08 -0400

    Updated test.py code
    Removed commented out code lines to make code
    cleaner and easier to read.
    
    Added a variable called 'data' to store the
    scraped data into for each scraper.get_data call.
    Also, added more print lines to show the type of
    variable data is and to format each iteration of
    the for loop nicer.

--- test.py
+++ test.py
@@ -1,24 +1,19 @@
 from redfin_scraper import RedfinScraper
 
-# scraper=RedfinScraper()
-# scraper.setup('./zip_code_sample.csv',multiprocessing=True)
-
 scraper = RedfinScraper()
 scraper.setup('./zip_code_sample.csv')
 
-# scraper.scrape(city_states=None,zip_codes=['JUNK'])
-# scraper.scrape(city_states=None,zip_codes='JUNK')
-# scraper.scrape(city_states=['Omaha, NE'],zip_codes='JUNK')
 scraper.scrape(city_states=None, zip_codes=['77002', '77003'])
 scraper.scrape(city_states=['Omaha,NE'], zip_codes=None)
-# scraper.scrape(city_states=[('Newark', 'NJ'),('Georgia', 'AL')],zip_codes=None)
-# scraper.scrape(city_states=['junk, junky'],zip_codes=['77002'])
-# scraper.scrape() # From Config
-# scraper.scrape(city_states=['Omaha,NE'],zip_codes=None,sold=True,sale_period='3mo')
+scraper.scrape(city_states=['Omaha,NE'],
+               zip_codes=None, sold=True, sale_period='3mo')
 
-for i in range(1, 3):
+for i in range(1, 4):
     try:
-        print(scraper.get_data(id=f"D00{i}"))
-        print("Success!")
+        data = scraper.get_data(id=f"D00{i}")
+        print(f"Scrape {i}: Success!")
+        print(type(data))
+        print(data)
     except:
         print("Failed")
+    print()
